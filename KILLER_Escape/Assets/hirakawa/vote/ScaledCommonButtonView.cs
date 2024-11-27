acd3172c2b6a9e5902b07b7d2aa1850f31844333
yuma <hsi47nrooafen@gmail.com>
2024-11-27 15:06:41 +0900

    [add] 汎用的なボタン

---
+++ KILLER_Escape/Assets/hirakawa/vote/ScaledCommonButtonView.cs
@@ -0,0 +1,50 @@
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CommonViewParts
+{
+    /// <summary>
+    /// 選択すると拡縮するようなボタンView
+    /// </summary>
+    [RequireComponent(typeof(CustomButton))]
+    public class ScaledCommonButtonView : MonoBehaviour
+    {
+        private const float DefaultScale = 1f;
+        private const float PressedScale = 0.9f;
+        
+        private const float ActiveImageAlpha = 1f;
+        private const float InactiveImageAlpha = 0.5f;
+        
+        private CustomButton _button;
+        [SerializeField] private Image _image;
+
+        [SerializeField] private bool isActive = false;
+
+        private void Start()
+        {
+            _button = GetComponent<CustomButton>();
+
+            _button.SetActive(isActive);
+            
+            _button.OnButtonPressed
+                .Subscribe(_ => SetScale(PressedScale))
+                .AddTo(this.gameObject);
+
+            _button.OnButtonReleased.Subscribe(_ => SetScale(DefaultScale)).AddTo(this.gameObject);
+            
+            _button.IsActiveRP.Subscribe(SetButtonActive).AddTo(this.gameObject);
+        }
+
+        private void SetScale(float scale)
+        {
+            _image.rectTransform.localScale = Vector3.one * scale;
+        }
+
+        private void SetButtonActive(bool isActive)
+        {
+            float alpha = isActive ? ActiveImageAlpha : InactiveImageAlpha;
+            _image.color = new Color(1, 1, 1, alpha);
+        }
+    }
+}
