e3f41dad32e39e3741704d54d7848ed8548e3689
Seto-VG <131129286+Seto-VG@users.noreply.github.com>
2024-12-11 14:45:20 +0900

    [update] カメラの切り替えと数秒待つ実装

--- KILLER_Escape/Assets/Scrips/State/PlayerActionState/ActionEnd.cs
+++ KILLER_Escape/Assets/Scrips/State/PlayerActionState/ActionEnd.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 public partial class GameManager
 {
-    private void StartActionEnd()
+    private async void StartActionEnd()
     {
         if (actionPlayerID == 3)
         {
             actionPlayerID = 0;
+            await WaitTime(2);
+            UpdateCamera();
         }
         else
         {
             actionPlayerID++;
+            await WaitTime(2);
+            UpdateCamera();
         }
     }
 
     private void UpdateActionEnd()
     {
         SetCurrentPlayerAction(GameConst.PlayerActionState.ACTION);
+    }
+
+    public async UniTask WaitTime(int time)
+    {
+        await UniTask.Delay(time * 1000);
     }
 }
